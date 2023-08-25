255a5f052ab3402b6f0f3a41d18e57fffaa64389
Clint Mourlevat <5221349+thisisclint21@users.noreply.github.com>
2023-08-25 21:00:02 +0200

    Cleaning

--- src/HypeProxy/Entities/BaseEntityWithCustomFilter.cs
+++ src/HypeProxy/Entities/BaseEntityWithCustomFilter.cs
@@ -1,10 +1,5 @@
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace HypeProxy.Entities;
 
 public class BaseEntityWithCustomFilter : BaseEntity
 {
-    [NotMapped]
-    [Obsolete]
-    public string PuteExpression { get; set; }
 }
