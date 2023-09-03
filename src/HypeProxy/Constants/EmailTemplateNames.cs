75f0b7c47e04e947fbc1adf1895e471868620ca6
Clint Mourlevat <5221349+thisisclint21@users.noreply.github.com>
2023-09-03 16:01:06 +0200

    Add `EmailTemplateNames`.

---
+++ src/HypeProxy/Constants/EmailTemplateNames.cs
@@ -0,0 +1,15 @@
+namespace HypeProxy.Constants;
+
+public enum EmailTemplateNames
+{
+    Welcome,
+    InvoicePaid,
+    PurchaseConfirmation,
+    OutdatedPaymentMethod,
+    GracePeriod,
+    AutomaticRenewalCancelled,
+    CancelledPurchase,
+    ForgotPassword,
+    ReviewUs,
+    Reminder,
+}
