9f06f2527543a5b89972630f294c6f58d7163b28
Clint Mourlevat <5221349+imclint21@users.noreply.github.com>
2023-11-03 09:21:07 +0100

    Add more properties to UserSummaryResponse

--- src/HypeProxy/Responses/UserSummaryResponse.cs
+++ src/HypeProxy/Responses/UserSummaryResponse.cs
@@ -7,9 +7,12 @@ namespace HypeProxy.Responses;
 public class UserSummaryResponse
 {
     public Guid UserId { get; set; }
+    public string? UserHash { get; set; }
     public string FullName { get; set; }
     public string? Email { get; set; }
     public string Avatar { get; set; }
     
+    public DateTime RegisteredAt { get; set; }
     public AuthorizationDetails? Authorization { get; set; }
+    public BillingDetails? BillingDetails { get; set; }
 }
