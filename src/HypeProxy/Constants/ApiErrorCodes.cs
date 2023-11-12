d8a6615ab2b1069cf41a7d40e97edf89480088ef
clint21.eth <me@clint21.dev>
2023-11-12 13:04:15 +0100

    Clean API error codes

--- src/HypeProxy/Constants/ApiErrorCodes.cs
+++ src/HypeProxy/Constants/ApiErrorCodes.cs
@@ -5,8 +5,8 @@ namespace HypeProxy.Constants;
 public struct ApiErrorCodes
 {
     public const string UnknownError = "UNKNOWN_ERROR";
-    public const string SuckDick = "SUCK_DICK";
     public const string PrivateApi = "PRIVATE_API";
+    public const string RateLimitReached = "RATE_LIMIT_REACHED";
 
     [TranspilationSource]
     public struct Authentication
@@ -18,34 +18,32 @@ public struct ApiErrorCodes
     
     public struct User
     {
-        public const string UserNotFound = "USER_NOT_FOUND";
-        public const string UserNotAllowed = "USER_NOT_ALLOWED";
+        public const string NotFound = "USER_NOT_FOUND";
+        public const string NotAllowed = "USER_NOT_ALLOWED";
         public const string MissingUserId = "MISSING_USER_ID";
     }
     
     public struct OrderValidation
     {
+        public const string UnprocessablePayment = "UNPROCESSABLE_PAYMENT";
+        public const string UnexpectedConditions = "UNEXPECTED_CONDITIONS";
         public const string FailedValidation = "FAILED_VALIDATION";
-        
-        [Obsolete]
-        public const string MissingFieldMaybe = "MAYBE_MISSING_FIELD";
-        
         public const string UnavailableQuantity = "UNAVAILABLE_QUANTITY";
-        public const string InvalidQuantity = "INVALID_QUANTITY";
         
         public const string InvalidPaymentMethod = "INVALID_PAYMENT_METHOD";
+        public const string InvalidBillingCycle = "INVALID_BILLING_CYCLE";
+        public const string InvalidQuantity = "INVALID_QUANTITY";
+        public const string InvalidCoupon = "INVALID_COUPON";
         
         public const string ProductNotFound = "PRODUCT_NOT_FOUND";
         public const string LocationNotFound = "LOCATION_NOT_FOUND";
         public const string ProviderNotFound = "PROVIDER_NOT_FOUND";
         
-        public const string InvalidCoupon = "INVALID_COUPON";
-        public const string UnprocessablePayment = "UNPROCESSABLE_PAYMENT";
-        public const string FuckCondition = "FUCK_CONDITION";
+        public const string IncompleteRequest = "INCOMPLETE_REQUEST";
     }
 
     public struct Features
     {
-        public const string UnavailableFeature = "NOT_AVAILABLE_FEATURE";
+        public const string UnavailableFeature = "UNAVAILABLE_FEATURE";
     }
 }
