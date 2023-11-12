2b6b7373631429b2ab2de32105475d5e04b15c3b
clint21.eth <me@clint21.dev>
2023-11-12 13:04:22 +0100

    Cleanup

--- src/HypeProxy/DataModels/GeolocationDetails.cs
+++ src/HypeProxy/DataModels/GeolocationDetails.cs
@@ -5,10 +5,10 @@ namespace HypeProxy.DataModels;
 [TranspilationSource]
 public class GeolocationDetails
 {
-    public string Continent { get; set; }
-    public string Country { get; set; }
-    public string CountryCode { get; set; }
-    public string CountryEmoji { get; set; }
-    public string Region { get; set; }
-    public string City { get; set; }
+    public string? Continent { get; set; }
+    public string? Country { get; set; }
+    public string? CountryCode { get; set; }
+    public string? CountryEmoji { get; set; }
+    public string? Region { get; set; }
+    public string? City { get; set; }
 }
