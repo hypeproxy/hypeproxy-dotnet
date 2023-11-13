bc9c7f7afb018171203fc1dfdc10ae103696f25b
clint21.eth <me@clint21.dev>
2023-11-13 18:46:09 +0100

    Bump to 3.0-alpha to 3.1-beta

--- .github/bump.sh
+++ .github/bump.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export MINOR_VERSION=$(grep -o "<Version>3.0.\(.*\)<\/Version>" ./src/HypeProxy/HypeProxy.csproj | sed 's/<Version>3.0.\(.*\)<\/Version>/\1/' | awk '{print $1+1}')
-sed -i "s/<Version>3.0.\(.*\)<\/Version>/<Version>3.0.$MINOR_VERSION<\/Version>/" ./src/HypeProxy/HypeProxy.csproj
-sed -i "s/<PackageVersion>3.0.\(.*\)-alpha<\/PackageVersion>/<PackageVersion>3.0.$MINOR_VERSION-alpha<\/PackageVersion>/" ./src/HypeProxy/HypeProxy.csproj
+export MINOR_VERSION=$(grep -o "<Version>3.1.\(.*\)<\/Version>" ./src/HypeProxy/HypeProxy.csproj | sed 's/<Version>3.1.\(.*\)<\/Version>/\1/' | awk '{print $1+1}')
+sed -i "s/<Version>3.1.\(.*\)<\/Version>/<Version>3.1.$MINOR_VERSION<\/Version>/" ./src/HypeProxy/HypeProxy.csproj
+sed -i "s/<PackageVersion>3.1.\(.*\)-beta<\/PackageVersion>/<PackageVersion>3.1.$MINOR_VERSION-beta<\/PackageVersion>/" ./src/HypeProxy/HypeProxy.csproj
